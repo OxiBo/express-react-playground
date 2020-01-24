7b6acdf70f5afdf2b737753cd4ed245b5d297323
OxiBo <xenos2000@ukr.net>
2020-01-24 17:13:50 -0500

    corrected typos in prod,js file

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  facebookAPIId: FACEBOOK_API_ID,
-  facebookAPISecret: FACEBOOK_API_SECRET,
+  facebookAPIId: process.env.FACEBOOK_API_ID,
+  facebookAPISecret: process.env.FACEBOOK_API_SECRET,
   facebookCallBackURL: process.env.FACEBOOK_CALLBACK_URL,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
